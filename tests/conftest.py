d11d3aefc85056703b2ff05fca5a55a46f05c477
Oksana <rosa111@mail.ru>
2023-06-06 16:13:56 +0700

    refactor

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,21 +9,3 @@ def browser():
     # driver.implicitly_wait(10)
     yield driver
     driver.quit()
-
-# @pytest.fixture(scope='session')
-# def db():
-#     # Подключение к базе данных
-#     db = PostgresqlDatabase(DB_NAME,
-#                             user=DB_USER,
-#                             password=DB_PASSWORD,
-#                             host=DB_HOST,
-#                             port=DB_PORT)
-#     connection = db.connection()
-#     return connection
-#
-#
-# @pytest.fixture(scope='session')
-# def db_cursor(db):
-#     # Курсор для выполнения операций с базой данных
-#     cursor = db.cursor()
-#     return cursor
